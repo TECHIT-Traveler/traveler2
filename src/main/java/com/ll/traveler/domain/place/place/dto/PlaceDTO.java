cea5a7dfabe3841198e69d02a3387bc792dd4f3d
silvercho <tjddms01212@gmail.com>
2024-03-14 17:33:33 +0900

    feat: 경기도 반려동물 동반 놀이터API

--- src/main/java/com/ll/traveler/domain/place/place/dto/PlaceDTO.java
+++ src/main/java/com/ll/traveler/domain/place/place/dto/PlaceDTO.java
@@ -6,14 +6,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PlaceDTO {
-    private String name;
-    private String adress;
-    private int latitude;
-    private int longitude;
-    private int number;
-    private String area;
-    private String homepage;
-    private String usageTime;
 
-
+    private String Type; // 업체구분
+    private String PlaceNm; // 업체명
+    private String landAddress; // 지번주소
+    private String loadAddress; // 도로명주소
+    private int latitude; // 위도
+    private int longitude; // 경도
+    private String phoneNumber; // 전화번호
+    
 }
---
+++ src/main/java/com/ll/traveler/domain/place/place/playGround/PlayGroundRepository.java
@@ -0,0 +1,7 @@
+package com.ll.traveler.domain.place.place.playGround;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayGroundRepository extends JpaRepository<PlayGround, Long> {
+
+}
