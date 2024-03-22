29a8de04374b25dc93770efdcb908474fa754e09
mklol00 <dlwodn97@naver.com>
2024-03-22 17:49:16 +0900

    feat: 강원도 반려동물 동반관광API

---
+++ src/main/java/com/ll/traveler/domain/place/place/gangwon2/Gangwon2.java
@@ -0,0 +1,26 @@
+package com.ll.traveler.domain.place.place.gangwon2;
+
+import com.ll.traveler.global.jpa.IdEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+@Getter
+@Setter
+public class Gangwon2 extends IdEntity {
+
+    private String 업체구분;
+    private String 업체명;
+    private String 지번_주소;
+    private String 도로명_주소;
+    private String 위도;
+    private String 경도;
+    private String 연락처;
+
+
+}
---
+++ src/main/java/com/ll/traveler/domain/place/place/gangwon2/Gangwon2ApiRepository.java
@@ -0,0 +1,7 @@
+package com.ll.traveler.domain.place.place.gangwon2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Gangwon2ApiRepository extends JpaRepository<Gangwon2, Long> {
+
+}
