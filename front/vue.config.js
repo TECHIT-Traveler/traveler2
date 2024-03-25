b1e9f29fabb9c1f3c250c825a424c9b65039f954
silvercho <tjddms01212@gmail.com>
2024-03-26 02:32:48 +0900

    test UI

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,5 +1,6 @@
 const bootstrapSassAbstractsImports = require("vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js");
 module.exports = {
+  buildDir: 'nuxt-dist',
   css: {
     loaderOptions: {
       sass: {
@@ -16,3 +17,13 @@ module.exports = {
     proxy: "http://localhost:8090",
   },
 };
+export default {
+  css: [
+    // Load a Node.js module directly (here it's a Sass file)
+    'bulma',
+    // CSS file in the project
+    '~/assets/css/main.css',
+    // SCSS file in the project
+    '~/assets/css/main.scss'
+  ]
+}
