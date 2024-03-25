1f674a39ce3f0f2431dfc7e326b2ed037014a9d7
silvercho <tjddms01212@gmail.com>
2024-03-25 10:07:40 +0900

    pull main

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Login from "@/components/Login";
+import cookies from "vue-cookies";
+import Join from "@/components/Join";
 import HelloWorld from "@/components/HelloWorld.vue";
 import Gyeonggi from "@/components/Gyeonggi.vue";
 import Gangwon from "@/components/Gangwon.vue";
 import Ulsan from "@/components/Ulsan.vue";
+import GangwonDetail from "@/components/GangwonDetail.vue";
+import MyPage from "@/components/MyPage.vue";
 
 Vue.use(Router);
+Vue.use(cookies);
 
 export default new Router({
   routes: [
@@ -28,6 +34,26 @@ export default new Router({
       path: "/Ulsan",
       name: "Ulsan",
       component: Ulsan,
+    },
+    {
+      path: "/gangwon2/:id",
+      name: "GangwonDetail",
+      component: GangwonDetail,
+    },
+    {
+      path: "/Login",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/Join",
+      name: "Join",
+      component: Join,
+    },
+    {
+      path: "/MyPage",
+      name: "MyPage",
+      component: MyPage,
     },
   ],
 });
