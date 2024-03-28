d147c725a303becb59f1e67ff4a276e112ba3863
caaano <wlsrl0515@naver.com>
2024-03-28 10:05:14 +0900

    fix: package.json

--- front/src/main.js
+++ front/src/main.js
@@ -6,8 +6,6 @@ import router from "./router";
 import axios from "axios";
 import BootstrapVue from "bootstrap-vue";
 import "bootswatch/dist/Zephyr/bootstrap.min.css";
-import './registerServiceWorkerDev'
-
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
